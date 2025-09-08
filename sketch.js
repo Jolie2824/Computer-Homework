784db252506965585b8d5763e720579740b146b7
Jolie2824 <Jolie.Padilla@mail.citytech.cuny.edu>
2025-09-07 20:35:38 -0400

    Computer Homework Stuff

---
+++ sketch.js
@@ -0,0 +1,40 @@
+/*
+////////////////////////////////////////
+////////Ghosts by Jolie Padilla/////////
+////////////////////////////////////////
+*/
+
+function setup() 
+{
+
+createCanvas(800, 800);
+
+background(150); //black and white background 0-255
+background (255, 0,255) //RGB background set
+
+//pancake
+
+Fill (255,255,0)
+
+elipse (100, 400, 200, 200)
+elipse (150, 450, 200, 200)
+elipse (200, 500, 200, 200)
+elipse (250, 550, 200, 200)
+}
+
+
+
+
+
+
+
+   
+    
+   
+    
+
+
+
+
+
+}
