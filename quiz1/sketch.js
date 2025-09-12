b4979a28ef6d9ef91dd99d3edaa3a044e1233525
Jolie2824 <Jolie.Padilla@mail.citytech.cuny.edu>
2025-09-12 12:07:26 -0400

    Quiz1

---
+++ quiz1/sketch.js
@@ -0,0 +1,20 @@
+
+/*
+////JolieP////
+//9/12/25//
+*/
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  //gray
+  background(127,127,127);
+
+//a red line
+fill (255,0,0); 
+
+line (400, 300, 0, 0);
+
+}
