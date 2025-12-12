5ddaa2dde4d96a2ae092fb7dd1b285640bb665d6
Jolie2824 <Jolie.Padilla@mail.citytech.cuny.edu>
2025-12-11 23:51:36 -0500

    Final_JolieP

--- FINAL_JolieP/sketch.js
+++ FINAL_JolieP/sketch.js
@@ -1,13 +1,8 @@
 /*
-////////////////////////////////////////
 ////////Ghosts by Jolie P/////////
-////////////////////////////////////////
 */
 
-//concept for future add score point,music, title, more imaged details, collsions and more cows for the hard mode
-
-comet= false;
-
+//concept for future add score point,music, more imaged details, hard mode
 let cometX = 753;
 
 let cometY = 50;
@@ -36,7 +31,7 @@ let beam;
 
 let grass;
 
-let cow;
+let sleepy; //cow
 
 let state = "homescreen"; //storing our states as Strings 
 
@@ -58,19 +53,29 @@ let savedTime = 0; // temp saved times, needed for comparison
 
 let cowUP= false;
 
-let cowX = 0; //variable for x value of ellipse (conspect for cow)
+let cowX = 0; //variable for x value of cow
 
-let cowY =550; 
+let cowY =505; 
 
 let increment = 60;	//variable for increment of x movement
 
-let cowSize = 100; //variable for circle diameter
+let cowSizew = 100; //variable for circle diameter
+
+let cowSizeh = 100; //variable for circle diameter
 
 let backtime;
 
 let wait = 1000; // change this to change the 'ticking'
 
+let Music;
+
+let ufos= [];
+
+let cows= [];
+
 let c;
+
+let data = { x: 550, y: 350, d: 10 };
 
 function preload () 
 {
@@ -81,9 +86,11 @@ aliens= loadImage ("images/aliens.png");
 
 beam= loadImage ("images/beam.png");
 
-cow= loadImage ("images/cow.png");
+sleepy= loadImage ("images/sleepy.png");
 
 grass= loadImage ("images/grass.gif");
+
+Music= loadSound ("music/Music.mp3");
 
 }
 
@@ -99,20 +106,36 @@ function setup() {
   textFont("Courier New", 25);
 
   time = millis(); // store the current time
-  //note: millis() returns the current number of milliseconds since starting the program.
-  //This information is often used for timing events and animation sequences.
-  //see http://p5js.org/reference/#/p5/millis
 
   c = color(255); // black
+
+
+// Add alien objects to the array.
+  
+  for (let i = 0; i < 5; i += 0.5) {
+    // Calculate random coordinates and size.
+    let x = random(0, 800);
+    let y = random(0, 800);
+    let s = random(5, 150);
+
+   // Create a new ufo object.
+    let ufo = new Ufo(x, y, s);
+
+    // Add the ufo to the array.
+    ufos.push(ufo);
+
+    // Create a new cow object.
+    let cow = new Cow(x, y, s);
+
+    // Add the ufo to the array.
+    cows.push(cow);
+  }
+  
 }
 
 function draw() 
 {
-homescreen(); 
-rules();
-game();
-gameOver();
- 
+
   if (state == "homescreen") 
 	{
     homescreen();
@@ -140,11 +163,12 @@ gameOver();
   }
 
   // use the boolean to control a function or behavior
-  if (cowUP) {
+  if (cowUP) 
+  {
     cowbeamed(); // only runs when inside the area
   }
 
-} 
+}
 
 function homescreen() 
 {
@@ -153,44 +177,155 @@ function homescreen()
   //check the difference between now and the previously stored time is greater than the wait interval
   if ((millis() - time) >= wait) 
     {
-    c = color(random(255), random(255), random(255)) //if it is, change the background color
+    c = color(random(255), random(255), random(255),100) //if it is, change the background color
     time = millis(); //also update the stored time
   }
   fill(0);
   textSize(46);
-  text("Cow Invaders", width/1.5, height/2.5);
+  text("Cow Invaders", width/2, height/2.5);
   textSize(34);
   fill(255);
   stroke(0,0,0,100);
   strokeWeight(4);
-  text("Click to start", width/1.6, height/2);
+  text("Click to start", width/2, height/2);
+
+  for (let ufo of ufos) 
+    {
+    // Show the ufo.
+    ufo.show();
+
+    // Move the ufo.
+    ufo.hop();
+
+    // Wrap around if they've hopped off the edge.
+    ufo.checkEdges();
+  }
+
+  for (let cow of cows) 
+    {
+    // Show the cow.
+    cow.show();
+
+    // Move the cow.
+    cow.hop();
+
+    // Wrap around if they've hopped off the edge.
+    cow.checkEdges();
+  }
+
+}
+
+  class Ufo 
+  {
+  constructor(x, y, size) 
+  {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+
+  show() 
+  {
+    textAlign(CENTER, CENTER);
+    textSize(this.size);
+    text('🛸', this.x, this.y);
+  }
+
+  hop() 
+  {
+    this.x += random(-50, 10);
+    this.y += random(-50, 10);
+  }
+
+  checkEdges() 
+    {
+      if (this.x > width) {
+      this.x = this.x - width;
+      } else if (this.x < 0) {
+      this.x = width - this.x;
+      }
+
+      if (this.y > height) {
+      this.y = this.y - height;
+      } else if (this.y < 0) {
+      this.y = height - this.y;
+      }
+    }
+      
+  }
+
+  class Cow 
+  {
+    constructor(x, y, size) 
+    {
+      this.x = x;
+      this.y = y;
+      this.size = size;
+    }
+
+  show() 
+  {
+    textAlign(CENTER, CENTER);
+    textSize(this.size);
+    text('🐮', this.x, this.y);
+  }
+
+  hop() 
+  {
+    this.x += random(-80, 60);
+    this.y += random(-80, 60);
+  }
+
+  checkEdges() 
+  {
+    if (this.x > width) {
+      this.x = this.x - width;
+    } else if (this.x < 0) {
+      this.x = width - this.x;
+    }
+
+    if (this.y > height) {
+      this.y = this.y - height;
+    } else if (this.y < 0) {
+      this.y = height - this.y;
+    }
+  }
+
 }
 
  function rules () 
  {
-  textSize(30);
   background (0,0,0);
-  fill(20, 200, 100, opacity);
-
         //aliens saying hello 
-    image (aliens, width - width / 2.6, height - height / 1.7, aliens.width / 1.2, aliens.height / 0.9); 
+        textAlign(CENTER, CENTER);
+        textSize(150);
+        // Access the object's values using the . operator.
+        text('👽',data.x, data.y, data.d);
+
+        // Update the object's x and y properties.
+        data.x += random(-5, 5);
+        data.y += random(-5, 5);
+
+  textSize(30);
+  
+  fill(20, 200, 100, opacity);
     text ("Hello humans",260, 25);
         //directions
     fill(20, 200, 100, opacity);
     text("How to save cows.", 350, 75);
     fill(255,255,0,opacity);
-    text("Grab 5 cows", 750, 500);
+    text("Grab 5 cows", 650, 500);
   //fade each text in 30seconds
 
-  text("Miss 3 cows you lose", 750, 550);
+  text("Miss 3 cows or the comet gameover", 450, 550);
 
-  text("Defeat the comet", 750, 600);
+  text("Defeat the comet", 600, 600);
 
-  text("Press the F key for lazer beam", 750, 650);
+  text("Press the F key for lazer beam", 500, 650);
 
-  text("Use the Up arrow key to collect the cows", 750, 700);
+  text("Use the Up arrow key to collect the cows", 400, 700);
 
-  text("Has to be done by the timer or you lose", 750, 750);
+  text("Has to be done by the timer or gameover", 400, 750);
 
   opacity= opacity + fade;
 
@@ -266,13 +401,10 @@ ellipse (305, 380, 15, 10);
 comet();
 
 //cow
-image (cow, width - width / 1, height - height / 3.2, cow.width / 2, cow.height / 2.5); //try to switch this over instead of the circle
-//fill(180,60,20); //color of the cow
+fill(180,60,20); //color of the cow
 
 currentTime = millis(); //update currentTime in draw so that it is continuously updating
-//image (cow, cowX,cowY,cowX, size)
-
-ellipse(cowX,cowY, height / 8, cowSize, cowSize);
+image (sleepy,cowX,cowY, cowSizew, cowSizeh); 
 
 cowX =cowX +5; // you can get the cow into the beam about 5-7 times
 
@@ -288,7 +420,7 @@ cowX =cowX +5; // you can get the cow into the beam about 5-7 times
 
     {
 
-      cowY = 550;
+      cowY = 505;
 
     }
      else if (cowY > 200)
@@ -330,14 +462,13 @@ image (grass, width- width /1, height- height /10, grass.width / 0.3, grass.heig
 
 //timing of crack in the ground by comet
 
-
-   if (comet ==false) 
+ if (cometX < 375 && cometY > 250 )
   {
 
-    crash();
+    image (cracked, width - width / 1.7, height - height / 3.2, cracked.width / 1.6, cracked.height / 2.5); 
 
   }
-/*
+
     if (lazerbeam && cometX > 250 && cometY > 350) 
        {
 
@@ -346,7 +477,7 @@ image (grass, width- width /1, height- height /10, grass.width / 0.3, grass.heig
         cometY = 50;
        
       }
-*/
+    
 lazerbeam();
 
 //topclouds
@@ -393,20 +524,6 @@ function comet ()
       cometX = width - size / 2; // exact top-right edge
       cometY = 0 - size / 2;     // start just above canvas
     }
-  
-
-
-}
-
-function crash ()
-{
-
- if (cometX < 375 && cometY > 250 )
-  {
-
-    image (cracked, width - width / 1.7, height - height / 3.2, cracked.width / 1.6, cracked.height / 2.5); 
-
-  }
 
 }
 
@@ -452,15 +569,17 @@ function gameOver()
 
 function mousePressed() 
 {
+  
   if (state == "homescreen") 
 	{
+    //Music.play();
 
     state = "rules";
 
   } 
    else if (state== "rules") 
   {
-
+    
     state = "game";
   }
   else if (state== "game") 
@@ -471,8 +590,9 @@ function mousePressed()
   }
 	else if (state == "gameOver") 
 	{
-
-    state = "homescreen";
     
+    state = "homescreen";
+    //Music.stop();
   }
 }
+
